051af2c42678f3e81a1bc6773d17c5323d8b52a0
Rajas Rakhe <rajas.rakhe@deeptek.ai>
2024-09-05 14:53:13 +0530

    Operators In Java

--- src/Java_Basic/Operators.java
+++ src/Java_Basic/Operators.java
@@ -7,6 +7,8 @@ public class Operators {
         // Arithmetic operators
         int a = 12;
         int b = 5 ;
+        boolean x = true;
+        boolean y = false;
 
         // Arithmetic Operator
         System.out.println("Arithmetic Operations (+,-,*,/,%)");
@@ -24,6 +26,12 @@ public class Operators {
 
         System.out.println("Postdecrement : " + (b--));
         System.out.println("Predecrement : " + (--b));
+
+        // Logical Operator
+        System.out.println("Logical Operations: ");
+        System.out.println(" Logical AND (x && y) : " + (x && y));
+        System.out.println(" Logical OR  (x || y) : " + (x || y));
+        System.out.println(" Logical NOT (!x) : " + (!x));
 
 
     }
