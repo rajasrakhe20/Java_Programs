0fc93c48f1fa513e90ff75530e24e810d3c5757d
Rajas Rakhe <rajas.rakhe@deeptek.ai>
2024-09-05 14:47:33 +0530

    Operators In Java

---
+++ src/Java_Basic/Operators.java
@@ -0,0 +1,30 @@
+package Java_Basic;
+
+public class Operators {
+
+    public static void main(String[] args) {
+
+        // Arithmetic operators
+        int a = 12;
+        int b = 5 ;
+
+        // Arithmetic Operator
+        System.out.println("Arithmetic Operations (+,-,*,/,%)");
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+
+
+        // Unary Operator
+        System.out.println("Unary Operations: ");
+        System.out.println("Postincrement : " + (a++));
+        System.out.println("Preincrement : " + (++a));
+
+        System.out.println("Postdecrement : " + (b--));
+        System.out.println("Predecrement : " + (--b));
+
+
+    }
+}
