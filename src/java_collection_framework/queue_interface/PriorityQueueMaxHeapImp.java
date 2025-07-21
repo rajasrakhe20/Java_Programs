cc33bcf46d9b2adfaabb7fed14440e6cf3716f1d
Rajas Rakhe <rajas.rakhe@deeptek.ai>
2025-07-21 17:51:01 +0530

    Priority Queue Max Heap in Java

---
+++ src/java_collection_framework/queue_interface/PriorityQueueMaxHeapImp.java
@@ -0,0 +1,21 @@
+package java_collection_framework.queue_interface;
+
+import java.util.* ;
+
+public class PriorityQueueMaxHeapImp {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq  = new PriorityQueue<>((Integer a, Integer b) -> b-a);  // Max Heap;
+        // Adding elements in priority queue
+        pq.add(5);
+        pq.add(2);
+        pq.add(3);
+        pq.add(1);
+        pq.add(4);
+
+        // Max Heap
+        System.out.println("Max Heap:");
+        while(!pq.isEmpty()){
+            System.out.println(pq.poll());
+        }
+    }
+}
